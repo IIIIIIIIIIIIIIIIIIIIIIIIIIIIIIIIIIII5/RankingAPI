ae0f12871ef1249a9c950aa94bc35a79e1008ae2
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-10-09 11:32:18 -0700

    Create config.js

---
+++ commands/config.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const { getJsonBin, saveJsonBin } = require("../utils");
+
+const CONFIG_CHANNEL_ID = "1423685663642877993";
+const ADMIN_ID = process.env.ADMIN_ID;
+const APPROVER_ID = "804292216511791204";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("config")
+        .setDescription("Set the group ID for this server")
+        .addIntegerOption(opt => opt.setName("groupid").setDescription("Roblox group ID").setRequired(true)),
+    async execute(interaction, PendingApprovals) {
+        const GroupId = interaction.options.getInteger("groupid");
+        const Db = await getJsonBin();
+        Db.ServerConfig = Db.ServerConfig || {};
+        Db.ServerConfig[interaction.guild.id] = { GroupId };
+        await saveJsonBin(Db);
+
+        PendingApprovals[GroupId] = { requesterId: interaction.user.id, guildId: interaction.guild.id };
+
+        const Channel = await interaction.client.channels.fetch(CONFIG_CHANNEL_ID);
+        const Row = new ActionRowBuilder().addComponents(
+            new ButtonBuilder().setCustomId(`accept_${GroupId}`).setLabel("Accept").setStyle(ButtonStyle.Success),
+            new ButtonBuilder().setCustomId(`decline_${GroupId}`).setLabel("Decline").setStyle(ButtonStyle.Danger)
+        );
+
+        await Channel.send({
+            content: `New pending config:\nGroup ID: ${GroupId}\nRequested by: <@${interaction.user.id}>`,
+            components: [Row]
+        });
+
+        await interaction.reply({ content: `Group ID **${GroupId}** set! Waiting for admin approval.`, ephemeral: true });
+    }
+};
