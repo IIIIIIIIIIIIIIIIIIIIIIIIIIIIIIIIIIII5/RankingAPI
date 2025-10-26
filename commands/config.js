a53be8a282f500a5e29db1993e382a8fb8def56f
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-10-26 10:21:24 -0400

    Update config.js

--- commands/config.js
+++ commands/config.js
@@ -1,9 +1,8 @@
 const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const { getJsonBin, saveJsonBin } = require("../utils");
+const { checkCommandRole } = require("../utils/roleCheck");
 
 const CONFIG_CHANNEL_ID = "1423685663642877993";
-const ADMIN_ID = process.env.ADMIN_ID;
-const APPROVER_ID = "804292216511791204";
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -14,42 +13,29 @@ module.exports = {
         ),
 
     async execute(interaction, verifications, PendingApprovals) {
-        if (!interaction.guild) {
-            return interaction.reply({
-                content: "This command can only be used in a server, not in DMs.",
-                ephemeral: true
-            });
-        }
+        const allowed = await checkCommandRole(interaction, "config");
+        if (!allowed) return interaction.reply({ content: "You don't have permission to use this command.", ephemeral: true });
+
+        if (!interaction.guild) return interaction.reply({ content: "This command can only be used in a server.", ephemeral: true });
 
         const GroupId = interaction.options.getInteger("groupid");
         const Db = await getJsonBin();
         Db.ServerConfig = Db.ServerConfig || {};
-        Db.ServerConfig[interaction.guild.id] = { GroupId };
+        Db.ServerConfig[interaction.guild.id] = { ...Db.ServerConfig[interaction.guild.id], GroupId };
         await saveJsonBin(Db);
 
         PendingApprovals[GroupId] = { requesterId: interaction.user.id, guildId: interaction.guild.id };
 
         const Channel = await interaction.client.channels.fetch(CONFIG_CHANNEL_ID).catch(() => null);
-        if (!Channel) {
-            return interaction.reply({
-                content: "Configuration channel not found. Please contact an admin.",
-                ephemeral: true
-            });
-        }
+        if (!Channel) return interaction.reply({ content: "Configuration channel not found.", ephemeral: true });
 
         const Row = new ActionRowBuilder().addComponents(
             new ButtonBuilder().setCustomId(`accept_${GroupId}`).setLabel("Accept").setStyle(ButtonStyle.Success),
             new ButtonBuilder().setCustomId(`decline_${GroupId}`).setLabel("Decline").setStyle(ButtonStyle.Danger)
         );
 
-        await Channel.send({
-            content: `New pending configuration:\nGroup ID: ${GroupId}\nRequested by: <@${interaction.user.id}>`,
-            components: [Row]
-        });
+        await Channel.send({ content: `New pending configuration:\nGroup ID: ${GroupId}\nRequested by: <@${interaction.user.id}>`, components: [Row] });
 
-        await interaction.reply({
-            content: `Group ID ${GroupId} set. Waiting for admin approval.`,
-            ephemeral: true
-        });
+        await interaction.reply({ content: `Group ID ${GroupId} set. Waiting for admin approval.`, ephemeral: true });
     }
 };
