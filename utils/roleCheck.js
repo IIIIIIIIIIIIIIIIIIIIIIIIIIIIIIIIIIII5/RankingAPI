ddf00b6d550246e3738418f3c90465e9d09c84a3
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-10-26 10:18:47 -0400

    Create roleCheck.js

---
+++ utils/roleCheck.js
@@ -0,0 +1,10 @@
+const { getJsonBin } = require("../utils");
+
+async function checkCommandRole(interaction, commandName) {
+    const Db = await getJsonBin();
+    const guildConfig = Db.ServerConfig?.[interaction.guild.id];
+    if (!guildConfig || !guildConfig.CommandRoles?.[commandName]) return true;
+
+    const roleId = guildConfig.CommandRoles[commandName];
+    return interaction.member.roles.cache.has(roleId);
+}
