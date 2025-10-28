535a2a6e92c95350559b45e25adea703398b8e1f
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-10-28 05:10:22 -0400

    Update shout.js

--- commands/shout.js
+++ commands/shout.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const { setGroupShout } = require("../roblox");
-const { getJsonBin } = require("../utils");
 const { checkCommandRole } = require("../roleCheck");
+const { logAction } = require("../logging");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,35 +11,36 @@ module.exports = {
 
     async execute(interaction) {
         const allowed = await checkCommandRole(interaction, "shout");
-        if (!allowed)
-            return interaction.reply({ content: "You don't have permission to use this command.", ephemeral: true });
-
-        const db = await getJsonBin();
-        if (!db.ServerConfig?.[interaction.guild.id])
-            return interaction.reply({ content: "Group ID not set. Run /config first.", ephemeral: true });
-
-        const groupId = db.ServerConfig[interaction.guild.id].GroupId;
-        const message = interaction.options.getString("message");
+        if (!allowed) return interaction.reply({ content: "You don't have permission to use this command.", ephemeral: true });
 
         try {
-            await setGroupShout(groupId, message);
+            const message = interaction.options.getString("message");
+            const Db = await require("../utils").getJsonBin();
+            const GroupId = Db.ServerConfig[interaction.guild.id].GroupId;
+
+            await setGroupShout(GroupId, message);
 
             const embed = new EmbedBuilder()
                 .setColor(0x3498db)
                 .setTitle("Group Shout Updated")
                 .addFields(
                     { name: "Message", value: message },
-                    { name: "Group ID", value: String(groupId), inline: true },
+                    { name: "Group ID", value: String(GroupId), inline: true },
                     { name: "Issued By", value: interaction.user.tag, inline: true },
                     { name: "Date", value: new Date().toISOString().split("T")[0], inline: true }
                 );
 
             await interaction.reply({ embeds: [embed] });
+            await logAction(interaction, embed);
         } catch (err) {
-            await interaction.reply({
-                content: `Failed to update shout: ${err.message || "Unknown error"}`,
-                ephemeral: true
-            });
+            const embed = new EmbedBuilder()
+                .setColor(0xe74c3c)
+                .setTitle("Failed to Update Shout")
+                .setDescription(err.message || "Unknown error")
+                .addFields({ name: "Date", value: new Date().toISOString().split("T")[0], inline: true });
+
+            await interaction.reply({ embeds: [embed], ephemeral: true });
+            await logAction(interaction, embed);
         }
     }
 };
