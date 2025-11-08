d20d1b205d49cc5a62de93989a959c522c1431bc
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-11-08 07:29:34 -0500

    Create profile.js

---
+++ commands/profile.js
@@ -0,0 +1,60 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { getRobloxUserId, getRobloxDescription, fetchRoles, getCurrentRank } = require("../roblox");
+const { GetJsonBin } = require("../utils");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("profile")
+        .setDescription("View a user's Roblox profile and XP")
+        .addStringOption(opt =>
+            opt.setName("username")
+               .setDescription("Roblox username")
+               .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        await interaction.deferReply();
+
+        const username = interaction.options.getString("username");
+        const Db = await GetJsonBin();
+
+        try {
+            const userId = await getRobloxUserId(username);
+            const robloxData = await getRobloxDescription(userId);
+
+            const guildId = interaction.guild.id;
+            const GroupId = Db.ServerConfig[guildId]?.GroupId;
+
+            let rank = "N/A";
+            if (GroupId) {
+                const userRank = await getCurrentRank(GroupId, userId);
+                rank = userRank;
+            }
+
+            const userXP = Db.XP?.[guildId]?.[userId]?.amount || 0;
+            const xpRanks = Db.XP?.[guildId]?.Ranks || {};
+            let nextRankXP = "Max";
+            const sortedXP = Object.entries(xpRanks).sort((a,b) => a[1] - b[1]);
+            for (const [rName, xpVal] of sortedXP) {
+                if (xpVal > userXP) {
+                    nextRankXP = xpVal - userXP;
+                    break;
+                }
+            }
+
+            const embed = new EmbedBuilder()
+                .setTitle(`${username} Profile`)
+                .setThumbnail(`https://www.roblox.com/headshot-thumbnail/image?userId=${userId}&width=150&height=150&format=png`)
+                .addFields(
+                    { name: "Username", value: username, inline: true },
+                    { name: "Rank", value: rank.toString(), inline: true },
+                    { name: "XP", value: userXP.toString(), inline: true },
+                    { name: "XP to Next Rank", value: nextRankXP.toString(), inline: true }
+                );
+
+            await interaction.editReply({ embeds: [embed] });
+        } catch (err) {
+            return interaction.editReply({ content: `Failed to fetch profile: ${err.message}` });
+        }
+    }
+};
