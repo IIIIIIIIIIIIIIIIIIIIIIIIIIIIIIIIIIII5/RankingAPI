23fd49dc0a9fd362ea80a30761223b9458da61e8
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-11-08 08:47:10 -0500

    Update xp.js

--- commands/xp.js
+++ commands/xp.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const { checkCommandRole } = require("../roleCheck");
-const { getJsonBin, saveJsonBin, GetRobloxUserId } = require("../utils");
+const { getJsonBin, saveJsonBin } = require("../utils");
+const { getRobloxUserId } = require("../roblox");
 const { fetchRoles } = require("../roblox");
 
 module.exports = {
@@ -34,7 +35,7 @@ module.exports = {
             .addChannelOption(opt => opt.setName("channel").setDescription("Channel to log XP").setRequired(true))),
 
     async execute(interaction) {
-        const Db = await GetJsonBin();
+        const Db = await getJsonBin();
         const guildId = interaction.guild.id;
 
         const sub = interaction.options.getSubcommand();
@@ -64,7 +65,7 @@ module.exports = {
             const amount = interaction.options.getInteger("amount");
 
             try {
-                const userId = await GetRobloxUserId(username);
+                const userId = await getRobloxUserId(username);
                 Db.XP = Db.XP || {};
                 Db.XP[guildId] = Db.XP[guildId] || {};
                 Db.XP[guildId][userId] = Db.XP[guildId][userId] || { amount: 0 };
@@ -72,7 +73,7 @@ module.exports = {
                 if (sub === "add") Db.XP[guildId][userId].amount += amount;
                 if (sub === "remove") Db.XP[guildId][userId].amount = Math.max(Db.XP[guildId][userId].amount - amount, 0);
 
-                await SaveJsonBin(Db);
+                await saveJsonBin(Db);
                 return interaction.reply({ content: `${sub === "add" ? "Added" : "Removed"} ${amount} XP ${sub === "add" ? "to" : "from"} ${username}.`, ephemeral: true });
             } catch (err) {
                 return interaction.reply({ content: `Failed: ${err.message}`, ephemeral: true });
@@ -87,7 +88,7 @@ module.exports = {
             Db.XP = Db.XP || {};
             Db.XP[guildId] = Db.XP[guildId] || {};
             Db.XP[guildId].PermissionRole = roleId;
-            await SaveJsonBin(Db);
+            await saveJsonBin(Db);
 
             return interaction.reply({ content: `XP permission role set to <@&${roleId}>`, ephemeral: true });
         }
@@ -100,7 +101,7 @@ module.exports = {
             Db.XP = Db.XP || {};
             Db.XP[guildId] = Db.XP[guildId] || {};
             Db.XP[guildId].Name = xpName;
-            await SaveJsonBin(Db);
+            await saveJsonBin(Db);
 
             return interaction.reply({ content: `XP name set to ${xpName}`, ephemeral: true });
         }
@@ -113,7 +114,7 @@ module.exports = {
             Db.XP = Db.XP || {};
             Db.XP[guildId] = Db.XP[guildId] || {};
             Db.XP[guildId].LogChannel = channel.id;
-            await SaveJsonBin(Db);
+            await saveJsonBin(Db);
 
             return interaction.reply({ content: `XP logs will now be sent in ${channel}`, ephemeral: true });
         }
