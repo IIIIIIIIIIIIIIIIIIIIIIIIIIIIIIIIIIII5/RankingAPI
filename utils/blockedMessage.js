2e1f735efe358569b7ea2303a9cef637947dc18a
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-11-27 13:19:58 -0500

    Create blockedMessage.js

---
+++ utils/blockedMessage.js
@@ -0,0 +1,34 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+
+async function sendBlockedMessage(interaction) {
+    const isUser = interaction.user && interaction.guild;
+    const blockedType = interaction.guild ? "server" : "user";
+
+    const row = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+            .setLabel("Contact Support")
+            .setStyle(ButtonStyle.Link)
+            .setURL("https://discord.gg/VhBqwBxHSd")
+    );
+
+    const messages = {
+        user: [
+            "You have been blocked from using the bot. For assistance, please contact support.",
+            "Access denied. Your account has been restricted. Reach out to our support team for help."
+        ],
+        server: [
+            "This server has been blocked from using the bot. Contact support for assistance.",
+            "Access to this server is restricted. Please get in touch with support if you believe this is an error."
+        ]
+    };
+
+    const message = messages[blockedType][Math.floor(Math.random() * messages[blockedType].length)];
+
+    if (interaction.replied || interaction.deferred) {
+        await interaction.editReply({ content: message, components: [row], ephemeral: true }).catch(() => {});
+    } else {
+        await interaction.reply({ content: message, components: [row], ephemeral: true }).catch(() => {});
+    }
+}
+
+module.exports = { sendBlockedMessage };
