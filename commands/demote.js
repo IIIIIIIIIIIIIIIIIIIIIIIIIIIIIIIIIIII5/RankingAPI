5c7b29d33616b26e045714390c588b1a6792f498
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-11-30 15:38:32 -0500

    Update demote.js

--- commands/demote.js
+++ commands/demote.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const { getCurrentRank, fetchRoles, setRank, getUserIdFromUsername } = require("../roblox");
+const { getCurrentRank, fetchRoles, setRank, GetUserIdFromUsername } = require("../roblox");
 const { checkCommandRole } = require("../roleCheck");
 const { logAction } = require("../logging");
 const { getJsonBin } = require("../utils");
